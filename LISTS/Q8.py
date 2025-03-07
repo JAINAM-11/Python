5785d62cf81a86bba455086c97a74a5ab2698c8f
JAINAM SHAH <109920340+JAINAM-11@users.noreply.github.com>
2025-03-07 13:19:43 +0530

    LISTS

---
+++ LISTS/Q8.py
@@ -0,0 +1,22 @@
+# 8. Rotate List
+# Problem: Write a function rotate_list(lst, k) that rotates a list lst to the right by k
+# positions. If k is greater than the length of the list, rotate it by k % len(lst) positions.
+
+# Jainam Shah
+# Date: 06/03/25
+
+def rotate_list(lst, k):
+    if not lst:  
+        return lst
+    k = k % len(lst)
+    return lst[-k:] + lst[:-k]
+
+user_input = input("Enter elements of the list separated by spaces: ")
+lst = []  
+
+for num in user_input.split(): 
+    lst.append(int(num)) 
+
+k = int(input("Enter the number of positions to rotate: "))
+
+print("Rotated List:", rotate_list(lst, k))
